fc2c32c0c5b1a3e85d95bcd5698651518e26d27c
Matt Saqueli <msaqueli22@turing.edu>
2023-01-13 12:45:10 -0600

    Add comments

--- challenge3.js
+++ challenge3.js
@@ -1,5 +1,7 @@
-
-
+// The goal is to target only the strings in the array that start with "T".
+// First I will build the array with strings, some that start with "T".
+// Next I will build a for loop to target all the strings in the array.
+// Then apply a if statement to target the words that start with "T".
 
 var foods = ["Tacos", "Burgers", "Steak", "Tamales"]
 for (var i = 0; i <foods.length; i++) {
