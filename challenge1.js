64b6baa6cb0f111f26fb2f5b6c53b1ca530535be
Matt Saqueli <msaqueli22@turing.edu>
2023-01-13 12:19:36 -0600

    Add completed answer

---
+++ challenge1.js
@@ -0,0 +1,9 @@
+
+
+
+var students = ["Matt", "John", "Brandon", "Chris"]
+for (var i = 0; i < students.length; i++) {
+    if (students[i].length === 4) {
+        console.log(students[i]);
+    }
+}
