9ce404b6a1935f52e9f4aa6f16d6347b5c619d30
Matt Saqueli <msaqueli22@turing.edu>
2023-01-13 12:40:19 -0600

    Add comments

--- challenge1.js
+++ challenge1.js
@@ -1,4 +1,7 @@
 
+// The overall goal is to only print the strings that have EXACTLY 4 characters.
+// I will first build my array. Then create a for statement to target the entire array.
+// Next I will include an if statement to only target the strings with exactly 4 characters.
 
 
 var students = ["Matt", "John", "Brandon", "Chris"]
